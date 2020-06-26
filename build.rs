a1c8ab5de7486dfcacd589679ecde91d332f2c42
Afnan Enayet <afnan@afnan.io>
2020-06-26 19:34:12 -0400

    Add submodules, update build file
    * Make build file dynamically source the `grammars` directory
    * Add git submodules for various tree sitter grammars

--- build.rs
+++ build.rs
@@ -1,26 +1,45 @@
 use cc;
-/// The build script for `diffsitter`
-///
-/// This compiles runtimes for each language so they can be used by the tree-sitter library.
-use std::path::PathBuf;
+use std::{fs, io, path::PathBuf};
 
 /// The top level directory for each language that contains the tree sitter source for each
 /// language
 static GRAMMARS_DIR: &'static str = "grammars";
 
-/// The valid source directories for tree sitter language runtimes for this program to compile
-static SOURCE_DIRECTORIES: &'static [&'static str] =
-    &["tree-sitter-rust", "tree-sitter-javascript"];
+/// Candidate source file names that might be in a tree sitter directory
+static SRC_FILE_CANDS: &'static [&'static str] = &["parser", "scanner"];
+
+/// Valid extensions for source files
+static VALID_EXTENSIONS: &'static [&'static str] = &["cc", "c"];
 
 fn main() {
-    for source_dir in SOURCE_DIRECTORIES {
-        let dir: PathBuf = [GRAMMARS_DIR, source_dir, "src"].iter().collect();
+    let grammars = fs::read_dir(GRAMMARS_DIR)
+        .unwrap()
+        .map(|res| res.map(|e| (e.file_name(), e.path())))
+        .collect::<Result<Vec<_>, io::Error>>()
+        .unwrap();
+
+    // Iterate through each grammar, find the valid source files that are in it, and add them as
+    // compilation targets
+    for grammar in grammars {
+        let output_name = grammar.0.to_string_lossy();
+        let dir = grammar.1.join("src");
+
+        // Take the cartesian product of the source names and valid extensions, and filter for the
+        // ones that actually exist in each folder
+        let build_files: Vec<PathBuf> = SRC_FILE_CANDS
+            .iter()
+            .flat_map(|&fname| {
+                VALID_EXTENSIONS
+                    .iter()
+                    .map(move |&ext| PathBuf::from(fname).with_extension(ext))
+            })
+            .map(|filename| dir.join(filename))
+            .filter(|candidate_file| candidate_file.is_file())
+            .collect();
 
         cc::Build::new()
             .include(&dir)
-            .file(dir.join("parser.c"))
-            .file(dir.join("scanner.c"))
-            .opt_level(3)
-            .compile(source_dir);
+            .files(build_files)
+            .compile(&output_name);
     }
 }
