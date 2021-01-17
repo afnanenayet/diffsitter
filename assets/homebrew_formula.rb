20d7d7b59dfaf4ccbda028f2f6d6d337b4fb913b
Afnan Enayet <afnan@afnan.io>
2021-01-17 02:10:04 -0600

    Add homebrew formula releaser
    * Add a github action to automatically bump the homebrew formula repo
    * Create a homebrew handlebar template for the action to use
    * Have the crate ignore assets, where the ruby template lives

---
+++ assets/homebrew_formula.rb
@@ -0,0 +1,12 @@
+class Diffsitter < Formula
+  desc "Meaningful semantic diffs"
+  homepage "https://github.com/afnanenayet/diffsitter"
+  license "MIT"
+  url "{{{tarballUrl}}}"
+  sha256 "{{{sha256}}}"
+  version "{{version}}"
+
+  def install
+    bin.install "diffsitter"
+  end
+end
