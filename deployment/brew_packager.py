82833ce310dd7b2754cfd334215a04f0523a01e7
Afnan Enayet <afnan@afnan.io>
2021-05-01 22:32:11 -0500

    Fix version string in release template
    Strip the "v" in a git tag, if applicable. This will fix an error in the
    Homebrew template where the inferred version from the URL leads to the
    wrong version being parsed.

--- deployment/brew_packager.py
+++ deployment/brew_packager.py
@@ -42,22 +42,44 @@ def sub_metadata(template_file: Path, package_meta: Dict[str, str]) -> str:
     with open(template_file) as f:
         template = Template(f.read())
 
-    return template.safe_substitute(
-        version=package_meta["version"],
-        shachecksum=package_meta["checksum"],
-    )
+    return template.safe_substitute(**package_meta)
+
+
+def filter_version(tag: str) -> str:
+    """Transform a version tag string to a proper semver version.
+
+    Version tags are usually of the form f"v{semver_version_string}". If that
+    is the case, this method will strip the version string of the leading "v".
+    If the string does not follow that convention, the string will not be
+    transformed.
+
+    Args:
+        tag: The git tag as a string
+
+    Returns:
+        The tag transformed into a SemVer compatible string.
+    """
+    if not tag:
+        return tag
+
+    if tag[0] == "v":
+        return tag[1:]
+
+    return tag
 
 
 def main():
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     release_version = sys.argv[3]
+    short_version = filter_version(sys.argv[3])
     checksum = sys.argv[4]
 
     print(f"Generating {output_file} from template {input_file}")
 
     metadata = {
         "version": release_version,
+        "short_version": short_version,
         "checksum": checksum,
     }
 
@@ -69,8 +91,10 @@ def main():
     output_contents = sub_metadata(Path(input_file), metadata)
 
     with open(output_file, "w") as out_fd:
+        print("Homebrew file:")
+        print(output_contents)
         out_fd.write(output_contents)
-        print(f"Generated {output_file}")
+        print(f"Wrote {output_file}")
 
 
 if __name__ == "__main__":
