eb53ef481319a92812fa63e653cb67dd887b6b7d
Afnan Enayet <afnanenayet@users.noreply.github.com>
2024-08-12 21:55:17 -0400

    Update treesitter grammars (#938)
    * chore(grammars): Update grammars
    
    * build: Update paths for ocaml grammar
    
    The ocaml grammar has a new structure.
    
    * test: Update snapshots
    
    Kind IDs have been updated because of grammar updates.

--- build.rs
+++ build.rs
@@ -275,7 +275,6 @@ fn grammars() -> Vec<GrammarCompileInfo<'static>> {
             display_name: "ocaml",
             path: PathBuf::from("grammars/tree-sitter-ocaml/grammars/ocaml"),
             c_sources: vec!["parser.c", "scanner.c"],
-            include_paths: Some(vec!["grammars/tree-sitter-ocaml/include".into()]),
             ..Default::default()
         },
         GrammarCompileInfo {
